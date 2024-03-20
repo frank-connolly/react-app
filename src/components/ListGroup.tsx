c233d09c455a49e9bac5d400ce07d063b835950b
Frank Connolly <frank.connolly@cronn.de>
2024-03-20 21:43:04 +0100

    feat: implement props for ListGroup with highlighted selected item

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,32 @@
-function ListGroup() {
-  const items = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
+import { useState } from "react";
+
+interface Props {
+  items: string[];
+  heading: string;
+}
+
+function ListGroup({ items, heading }: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+      <h1>{heading}</h1>
+      {items.length === 0 && "No items to display"}
       <ul className="list-group">
-        {items.map((item) => (
-          <li>{item}</li>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
